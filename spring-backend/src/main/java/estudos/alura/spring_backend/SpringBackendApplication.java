9b2d9f4a6f316f518a629055a140ba326486fd4b
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-06 21:04:55 -0300

    Estudando SPRINGBOOT [somente backend]

---
+++ spring-backend/src/main/java/estudos/alura/spring_backend/SpringBackendApplication.java
@@ -0,0 +1,19 @@
+package estudos.alura.spring_backend;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello Word!");
+        System.out.println("SpringBoot -> Backend");
+    }
+}
