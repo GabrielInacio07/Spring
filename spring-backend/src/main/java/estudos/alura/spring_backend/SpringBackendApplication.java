cf518a265b816ff662d25199203651698fee8725
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-08 12:22:35 -0300

    Modelando a Temporada

--- spring-backend/src/main/java/estudos/alura/spring_backend/SpringBackendApplication.java
+++ spring-backend/src/main/java/estudos/alura/spring_backend/SpringBackendApplication.java
@@ -3,6 +3,7 @@ package estudos.alura.spring_backend;
 import estudos.alura.spring_backend.model.Episode;
 import estudos.alura.spring_backend.model.Serie;
 import estudos.alura.spring_backend.model.Tarefa;
+import estudos.alura.spring_backend.model.Temporada;
 import estudos.alura.spring_backend.service.ConsumerAPI;
 import estudos.alura.spring_backend.service.ConvertDados;
 import org.springframework.boot.CommandLineRunner;
@@ -10,6 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBackendApplication implements CommandLineRunner {
@@ -27,15 +30,22 @@ public class SpringBackendApplication implements CommandLineRunner {
         ConvertDados converter = new ConvertDados();
 
         Serie serie = converter.obterDados(json,Serie.class);
-        System.out.println(serie);
+        //System.out.println(serie);
 
-            json = consumerapi.obterDados("https://www.omdbapi.com/?t=vikings&Season=2&episode=6&apikey=d7b5168c");
+        json = consumerapi.obterDados("https://www.omdbapi.com/?t=vikings&Season=2&episode=6&apikey=d7b5168c");
         Episode episode = converter.obterDados(json,Episode.class);
-        System.out.println(episode);
+        //System.out.println(episode);
 
+        List<Temporada> listTemporada = new ArrayList<>();
 
+        for (int i = 1; i <= serie.totalTemporadas(); i++) {
+            json = consumerapi.obterDados("https://www.omdbapi.com/?t=vikings&Season=" + i + "&apikey=d7b5168c");
+            Temporada temp = converter.obterDados(json, Temporada.class);
+            listTemporada.add(temp);
 
+        }
 
+        listTemporada.forEach(System.out::println);
     }
 
 }
---
+++ spring-backend/src/main/java/estudos/alura/spring_backend/model/Temporada.java
@@ -0,0 +1,12 @@
+package estudos.alura.spring_backend.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Temporada(
+        @JsonAlias("Season") Integer numero,
+        @JsonAlias("Episodes") List<Episode> episodes) {
+}
