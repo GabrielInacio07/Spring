d3c0029ecbe0075087e8c4da4a2ef0cdf031c179
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-11 00:24:16 -0300

    Refatorando código

--- spring-backend/src/main/java/estudos/alura/spring_backend/model/Menu.java
+++ spring-backend/src/main/java/estudos/alura/spring_backend/model/Menu.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import estudos.alura.spring_backend.service.ConsumerAPI;
 import estudos.alura.spring_backend.service.ConvertDados;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -18,95 +17,68 @@ public class Menu {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=d7b5168c";
 
+    private List<Serie> listaSeries = new ArrayList<>();
+
     public void exibirMenu() throws JsonProcessingException {
-        System.out.print("Digite o nome da serie: ");
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                     1 - Adicionar séries
+                     2 - Buscar episódios
+                     3 - Listar séries buscadas
+                     0 - Sair
+                    """;
 
-        String nome = input.nextLine();;
-        var json = consumerapi.obterDados(ENDERECO + nome.trim().replace(" " , "+") + API_KEY);
+            System.out.println(menu);
+            opcao = input.nextInt();
+            input.nextLine();
 
-        Serie serie = converterDados.obterDados(json,Serie.class);
-        System.out.println(serie);
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSerieBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
 
-        List<Temporada> listTemporada = new ArrayList<>();
+            }
+        }
+    }
 
-        for (int i = 1; i <= serie.totalTemporadas(); i++) {
-            json = consumerapi.obterDados(ENDERECO + nome.replace(" " , "+") + "&Season=" + i + API_KEY);
-            Temporada temp = converterDados.obterDados(json, Temporada.class);
-            listTemporada.add(temp);
+    private void buscarSerieWeb() throws JsonProcessingException {
+        Serie dados = getDadosSeries();
+        listaSeries.add(dados);
+    }
 
+    private Serie getDadosSeries() throws JsonProcessingException {
+        System.out.print("Digite o nome da série: ");
+        String nome = input.nextLine();
+        String json = consumerapi.obterDados(ENDERECO + nome.trim().replace(" ", "+") + API_KEY);
+        return converterDados.obterDados(json, Serie.class);
+    }
+
+    private void buscarEpisodioPorSerie() throws JsonProcessingException {
+        Serie dadosSerie = getDadosSeries();
+        List<Temporada> temporadas = new ArrayList<>();
+
+        for (int i = 0; i <= dadosSerie.totalTemporadas(); i++) {
+            String json = consumerapi.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + API_KEY);
+            Temporada dadosTemporada = converterDados.obterDados(json, Temporada.class);
+            temporadas.add(dadosTemporada);
         }
 
+        temporadas.forEach(System.out::println);
+    }
 
-
-        List<Episode> listaEpisodios = listTemporada
-                .stream()
-                .flatMap(temp -> temp.episode().stream()).toList();
-
-        //Filtrando por avaliação
-        System.out.println("Top 5 ep:");
-        listaEpisodios.stream()
-                .filter(ep ->  ep.avaliacao() != null && !ep.avaliacao().isEmpty() && !ep.avaliacao().equals("N/A"))
-                .sorted(Comparator.comparing(Episode::avaliacao).reversed())
-                .limit(5)
-                .forEach(System.out::println);
-
-
-              //Filtrando por ano de lançamento
-        List<Episodio_item> dadosEpisodio = listTemporada.stream()
-                .flatMap(temporada -> temporada.episode().stream()
-                        .map(episode -> new Episodio_item(temporada.numero(), episode)))
-                .toList();
-
-        dadosEpisodio.forEach(System.out::println);
-
-        System.out.println("Digite o episódio desejado: ");
-        String trechoTitulo = input.nextLine();
-        Optional<Episodio_item> episodioBuscado = dadosEpisodio.stream()
-                .filter(episodioItem -> episodioItem.getTitulo()
-                        .toUpperCase().contains(trechoTitulo.toUpperCase()))
-                .findFirst();
-
-        if(episodioBuscado.isPresent()){
-            System.out.println("Episódio encontrado!");
-            System.out.println("Temporada: " + episodioBuscado.get());
-        }else{
-            System.out.println("Episódio não encontrado");
-        }
-
-        Map<Integer, Double> avaliacaoTemporada = dadosEpisodio
-                .stream()
-                .filter(episodioItem -> episodioItem.getAvaliacao() > 0.0)
-                .collect(Collectors.groupingBy(Episodio_item::getTemporada,
-                        Collectors.averagingDouble(Episodio_item::getAvaliacao)));
-        System.out.println(avaliacaoTemporada);
-
-        DoubleSummaryStatistics estatics = dadosEpisodio
-                .stream()
-                .filter(episodioItem -> episodioItem.getAvaliacao() > 0.0)
-                .collect(Collectors.summarizingDouble(Episodio_item::getAvaliacao));
-
-        System.out.println("Média: " + estatics.getAverage());
-        System.out.println("Melhor episódio: " + estatics.getMax());
-        System.out.println("Pior episódio: " + estatics.getMin());
-        System.out.println("Quantia de de episódio: " + estatics.getCount());
-
-//
-//        System.out.println("Digite a filtragem por an, dos episódios: ");
-//        int ano = Integer.parseInt(input.nextLine());
-//
-//        LocalDate dataBusca = LocalDate.of(ano,1,1);
-//
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//
-//
-//        dadosEpisodio
-//                .stream()
-//                .filter(episodioItem -> episodioItem.getDataLancamento() != null
-//                        && episodioItem.getDataLancamento().isAfter(dataBusca))
-//                .forEach(e -> System.out.println(
-//                        "Temporada: " + e.getTemporada() +
-//                                " Episodio: " + e.getTitulo() +
-//                                " Data Lançamento: " + e.getDataLancamento().format(formatter)
-//                ));
+    private void listarSerieBuscadas() {
+        listaSeries.forEach(System.out::println);
     }
 }
--- spring-backend/src/main/java/estudos/alura/spring_backend/model/Serie.java
+++ spring-backend/src/main/java/estudos/alura/spring_backend/model/Serie.java
@@ -8,6 +8,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record Serie(
         @JsonAlias("Title") String titulo,
         @JsonAlias("totalSeasons") Integer totalTemporadas,
-        @JsonAlias("imdbRating") String avaliacao){//le o JSON e insere o atributo com mesmo nome do JSON//////
+        @JsonAlias("imdbRating") String avaliacao,
+        @JsonAlias("Genre")String genero,
+        @JsonAlias("Actors")String atores,
+        @JsonAlias("Poster")String poster,
+        @JsonAlias("Plot") String sinopse){//le o JSON e insere o atributo com mesmo nome do JSON//////
 
+
+    @Override
+    public String toString() {
+        return "{\n" +
+                "  \"titulo\": \"" + titulo + "\",\n" +
+                "  \"totalTemporadas\": " + totalTemporadas + ",\n" +
+                "  \"avaliacao\": \"" + avaliacao + "\",\n" +
+                "  \"genero\": \"" + genero + "\",\n" +
+                "  \"atores\": \"" + atores + "\",\n" +
+                "  \"poster\": \"" + poster + "\",\n" +
+                "  \"sinopse\": \"" + sinopse + "\"\n" +
+                "}";
+    }
 }
